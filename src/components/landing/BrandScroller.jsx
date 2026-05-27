765bf6a76bbea2801149814c493ce95d81d931f2
[dyad] <hitensapra44@gmail.com>
2026-05-28 00:00:37 +0530

    Updated src/components/landing/BrandScroller.jsx

--- src/components/landing/BrandScroller.jsx
+++ src/components/landing/BrandScroller.jsx
@@ -48,7 +48,7 @@ export default function BrandScroller() {
           textTransform: 'uppercase',
           margin: 0
         }}>
-          1000+ SaaS<span style={{ color: '#9C2000', fontWeight: 700 }}> Automated</span> Their Marketing
+          style={{ color: '#9C2000', fontWeight: 700 }}1000+ SaaS <span> Automated</span> Their Marketing
         </p>
       </div>
 
