bbc24c36d39d5bfab2ad0512c172ea6151a13ff2
[dyad] <hitensapra44@gmail.com>
2026-06-21 20:24:53 +0530

    [dyad] add Dyad component tagger

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,7 +13,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
+  plugins: [dyadComponentTagger(), react()],
   server: {
     port: 5173,
   }
